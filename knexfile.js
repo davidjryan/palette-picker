3ec31effd863dc2eeb7ebfdf706058df8e319335
David Ryan <djrdev@icloud.com>
2018-01-25 15:19:23 -0700

    added migrations and seeds

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
     connection: {
       filename: 'postgres://localhost/palette'
     },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seed: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
   }
 };
