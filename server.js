6a61c78f19ae7dd2b2a89e5d73f782af31faa844
David Ryan <djrdev@icloud.com>
2018-01-25 17:59:00 -0700

    added project and palette end points to server

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// We want to know if we're in development, testing, or production environment.  
+// If we don't know, we'll assume we're in development.
+const environment = process.env.NODE_ENV || 'development';
+
+// Based on that environment, we'll fetch the database configuration from knexfile.js 
+// for whatever environment we're in  and now our express app will be able to connect to it.
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
-
-app.locals.title = 'Palette Picker'
-app.locals.projects = [
-  { project: 'sweet', palette: [ '#FE5fsd', '#FED354', '#FEG345', '#FHG678', '#FTY856']}
-]
 
 app.get('/', (request, response) => {
   response.send('what up color boy')
 });
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then((projects) => {
+      response.status(200).json(projects);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then((palettes) => {
+      response.status(200).json(palettes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
