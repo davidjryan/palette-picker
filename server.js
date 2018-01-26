466a1a79ae8dae9732209faa94335b1a18884cfe
David Ryan <djrdev@icloud.com>
2018-01-25 21:11:04 -0700

    added post

--- server.js
+++ server.js
@@ -45,18 +45,42 @@ app.post('/api/v1/projects', (request, response) => {
       return response
         .status(422)
         .send({ error: `Expected format: { project: <String> }. 
-          You're missing a "${requiredParameter}" property.` });
+          You're missing a "${requiredParameter}" property.`
+        });
     }
   }
 
   database('projects').insert(project, 'id')
-    .then(paper => {
-      response.status(201).json({ id: paper[0] })
+    .then(project => {
+      response.status(201).json({ id: project[0] })
     })
     .catch(error => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/projects/:id/palettes', (request, response) => {
+  // grab project id
+  const { id } = request.params;
+  // combine palette object with project id reference
+  const palette = Object.assign({}, request.body, { project_id: id })
+  // check for missing params
+  for (let requiredParams of ['project', 'palette', 'hex1', 'hex2', 'hex3', 'hex4', 'hex5']) {
+    if (!palette[requiredParams]) {
+      return response
+        .status(422)
+        .json({ error: `You are missing ${requiredParams}` })
+    }
+  }
+  // insert palette
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      return response.status(201).json({ id: palette[0] })
+    })
+    .catch(error => {
+      return response.status(500).json({ error: 'yay error' })
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
