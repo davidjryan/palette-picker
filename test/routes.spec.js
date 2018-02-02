8fd32bd9b03cb4c73585580f05d93ddb0fbc1b56
David Ryan <djrdev@icloud.com>
2018-02-02 11:52:18 -0700

    tests

---
+++ test/routes.spec.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the homepage with text', () => {
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+        response.res.text.should.equal('what up color boy');
+      })
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(err => {
+        throw err;
+      });
+  });
+});
+describe('API Routes', () => {
+
+});
